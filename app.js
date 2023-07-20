7455cb02b58e05d1ae81a5b51c6257c1d0859831
alielrayes <engali7assan@yahoo.com>
2023-07-20 05:18:28 +0300

    lesson3

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const port = 3001;
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", {root: __dirname})
-})
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+
+
+mongoose
+  .connect(
+    "mongodb+srv://devali:Y3mkQvum8gItJ861@cluster0.5cqribi.mongodb.net/all-data?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
