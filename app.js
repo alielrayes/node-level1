cfc1ea310f2e0fd73de834b87245192cd14c89e3
alielrayes <engali7assan@yahoo.com>
2023-07-25 04:18:19 +0300

    lesson6

--- app.js
+++ app.js
@@ -4,13 +4,49 @@ const port = 3001;
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 const Mydata = require("./models/mydataSchema");
+app.set("view engine", "ejs");
+app.use(express.static('public'))
+
+
+// Auto refresh
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+ 
+ 
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+ 
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+
+
+
+
+
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/home.html", { root: __dirname });
+// result ==> array of objects
+
+  Mydata.find()
+    .then((result) => {
+      res.render("home", { mytitle: "Home page", arr: result });
+   
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  
 });
 
 app.get("/index.html", (req, res) => {
-  res.send("<h1>  تم ارسال البيانات بنجاح </h1>")
+  res.send("<h1>  تم ارسال البيانات بنجاح </h1>");
 });
 
 mongoose
@@ -31,9 +67,12 @@ app.post("/", (req, res) => {
 
   const mydata = new Mydata(req.body);
 
-  mydata.save().then(() => {
-    res.redirect("/index.html");
-  }).catch((err) => {
-    console.log(err)
-  });
+  mydata
+    .save()
+    .then(() => {
+      res.redirect("/index.html");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
