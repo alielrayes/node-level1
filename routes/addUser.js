f0338f03c5d266766c86fd87865f1cf438cee607
alielrayes <engali7assan@yahoo.com>
2023-08-01 09:44:00 +0300

    lesson19

--- routes/addUser.js
+++ routes/addUser.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/customerSchema");
+const userController = require("../controllers/userController");
 var moment = require("moment");
 
-router.get("", (req, res) => {
-  res.render("user/add");
-});
+router.get("", userController.user_add_get);
 // POST Requst
-router.post("", (req, res) => {
-  User.create(req.body)
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.post("", userController.user_post);
 
 module.exports = router;
--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -2,77 +2,21 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/customerSchema");
 var moment = require("moment");
-
+const userController = require("../controllers/userController");
 // GET Requst
-router.get("/", (req, res) => {
-  // result ==> array of objects
-  console.log("--------------------------------------------");
-  User.find()
-    .then((result) => {
-      res.render("index", { arr: result, moment: moment });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
 
-router.get("/edit/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((result) => {
-      res.render("user/edit", { obj: result, moment: moment });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/", userController.user_index_get);
 
-router.get("/view/:id", (req, res) => {
-  // result ==> object
-  User.findById(req.params.id)
-    .then((result) => {
-      res.render("user/view", { obj: result, moment: moment });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/edit/:id", userController.user_edit_get);
 
-router.post("/search", (req, res) => {
-  console.log("*******************************");
+router.get("/view/:id", userController.user_view_get);
 
-  const searchText = req.body.searchText.trim();
-
-  User.find({ $or: [{ fireName: searchText }, { lastName: searchText }] })
-    .then((result) => {
-      console.log(result);
-      res.render("user/search", { arr: result, moment: moment });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.post("/search", userController.user_search_post);
 
 // DELETE Request
-router.delete("/edit/:id", (req, res) => {
-  User.deleteOne({ _id: req.params.id })
-    .then((result) => {
-      res.redirect("/");
-      console.log(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.delete("/edit/:id", userController.user_delete);
 
 // PUT Requst
-router.put("/edit/:id", (req, res) => {
-  User.updateOne({ _id: req.params.id }, req.body)
-    .then((result) => {
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.put("/edit/:id", userController.user_put);
 
 module.exports = router;
